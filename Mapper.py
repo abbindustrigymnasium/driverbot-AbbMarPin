7b7874ee41135dec2655182cfa928452e5b80e7e
AbbMarPin <54662893+AbbMarPin@users.noreply.github.com>
2020-06-02 08:35:39 +0200

    kommentar

--- Mapper.py
+++ Mapper.py
@@ -17,7 +17,7 @@ ang_mult = 1
 updatefreq = 0.35
 hjul_bas = 9.5
 
-pos = {
+pos = { # position objeckt som används för att styra bilen
     "ang": 0,
     "armed": False,
     "speed": 0,
@@ -37,7 +37,7 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     global pos
 
-    if msg.topic == username + "/motor":
+    if msg.topic == username + "/motor": # läser in meddelandet
         res = eval(str(msg.payload))
         pos["armed"] = res[0]
         pos["dir"] = res[1]
@@ -49,16 +49,16 @@ def on_message(client, userdata, msg):
         pos["ang"] = ang * ang_mult
 
 
-def map_range(value, low1, high1, low2, high2):
+def map_range(value, low1, high1, low2, high2): # map range som i arduino
     return low2 + ((high2 - low2) * (value - low1)) / (high1 - low1)
 
 
-def loop():
+def loop(): # rita ut allt
     print("loop", pos)
     # print(pos["speed"])
-    if pos["armed"]:
+    if pos["armed"]: # om den är på
 
-        if pos["ang"] != 0:
+        if pos["ang"] != 0: # radie om den svänger
             Radie = (hjul_bas/math.sin(pos["ang"])) *- 1
             if pos["dir"]:
                 turt.circle(Radie, pos["speed"])
@@ -66,7 +66,7 @@ def loop():
                 turt.circle(Radie, pos["speed"]*-1)
         else:
             if pos["dir"]:
-                turt.fd(pos["speed"])
+                turt.fd(pos["speed"]) # fram / bak
             else:
                 turt.fd(pos["speed"]*-1)
 
@@ -76,15 +76,12 @@ def loop():
 def main():
     client = mqtt.Client()
     client.on_connect = on_connect
-    client.on_message = on_message
+    client.on_message = on_message # mqtt saker
     client.username_pw_set(username, password)
     client.connect("maqiatto.com", 1883, 60)
     client.loop_start()
     while 1:
         time.sleep(updatefreq)
         loop()
-
-
-
 
 main()
